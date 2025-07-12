4dd1e7a760aa748324223798fcc7cfee02ed2834
Yousef Emad <yousefemadkhazbak@gmail.com>
2025-07-12 19:54:36 +0300

    add requeste inspecotrs package. add sensor plus package. add shake gesture package.

--- lib/Home_screen.dart
+++ lib/Home_screen.dart
@@ -1,38 +1,25 @@
-import 'package:auth_feature/feature/sign_out/sign_out_screen.dart';
+
+import 'package:auth_feature/sensor_mobile.dart';
+import 'package:auth_feature/shake_gesture_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-import 'core/theming/app_styles.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Column(
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Screen')),
+
+      body: const Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Column(
+          ShakeGestureWidget(),
+          SensorMobile(),
 
-              children: [
-                Center(
-                  child: Text(
-                    'Never Give Up !!!!!!!!!!!!!! ',
-                    style: AppStyles.font20Blue,
-                  ),
-                ),
-                SizedBox(height: 18.h,),
-                const SignOutScreen(),
-              ],
-            ),
-          ),
+          // This causes the overflow
         ],
-
       ),
     );
   }
--- lib/feature/sign_up/presentation/screen/sign_up_screen.dart
+++ lib/feature/sign_up/presentation/screen/sign_up_screen.dart
@@ -2,6 +2,7 @@ import 'package:auth_feature/core/routing/routers.dart';
 import 'package:auth_feature/core/widgets/app_text_button.dart';
 import 'package:auth_feature/feature/sign_up/cubit/sign_up_cubit.dart';
 import 'package:auth_feature/feature/sign_up/presentation/widget/sign_up_form.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -14,64 +15,67 @@ class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Sign Up')),
-      body: BlocProvider(
-        create: (context) => SignUpCubit(),
-        child: BlocListener<SignUpCubit, SignUpState>(
-          listener: (context, state) {
-            if (state is SignUpSuccessState) {
-              Navigator.pushReplacementNamed(context, Routers.homeRouter);
-            }
-          },
-          child: BlocBuilder<SignUpCubit, SignUpState>(
-            builder: (context, state) {
-              if (state is SignUpLoadingState) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (state is SignUPErrorState) {
-                return Center(child: Text(state.error));
+      
+      body: SafeArea(
+        child: BlocProvider(
+          create: (context) => SignUpCubit(),
+          child: BlocListener<SignUpCubit, SignUpState>(
+            listener: (context, state) {
+              if (state is SignUpSuccessState) {
+                Navigator.pushReplacementNamed(context, Routers.homeRouter);
               }
-
-              return ListView(
-                padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 20.h),
-                children: [
-                  const SignUpForm(),
-                  SizedBox(height: 18.h),
-                  ElevatedButton(
-                    style: AppStyles.elevatedButtonStyle,
-                    onPressed: state is SignUpLoadingState
-                        ? null
-                        : () {
-                            if (context
-                                    .read<SignUpCubit>()
-                                    .formKey
-                                    .currentState
-                                    ?.validate() ==
-                                true) {
-                              context.read<SignUpCubit>().signUp(
-                                email: context
-                                    .read<SignUpCubit>()
-                                    .emailController
-                                    .text,
-                                password: context
-                                    .read<SignUpCubit>()
-                                    .passwordController
-                                    .text,
-                              );
-                            }
-                          },
-                    child: const Text('Register'),
-                  ),
-                  SizedBox(height: 10.h),
-                  AppTextButton(
-                    text: 'OR Sign In',
-                    textStyle: AppStyles.font14Blue,
-                    onPressed: () {
-                      Navigator.pushNamed(context, Routers.signInRouter);
-                    },
-                  ),
-                ],
-              );
             },
+            child: BlocBuilder<SignUpCubit, SignUpState>(
+              builder: (context, state) {
+                if (state is SignUpLoadingState) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (state is SignUPErrorState) {
+                  return Center(child: Text(state.error));
+                }
+        
+                return ListView(
+                  padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 20.h),
+                  children: [
+                    const SignUpForm(),
+                    SizedBox(height: 18.h),
+                    ElevatedButton(
+                      style: AppStyles.elevatedButtonStyle,
+                      onPressed: state is SignUpLoadingState
+                          ? null
+                          : () {
+                              if (context
+                                      .read<SignUpCubit>()
+                                      .formKey
+                                      .currentState
+                                      ?.validate() ==
+                                  true) {
+                                context.read<SignUpCubit>().signUp(
+                                  email: context
+                                      .read<SignUpCubit>()
+                                      .emailController
+                                      .text,
+                                  password: context
+                                      .read<SignUpCubit>()
+                                      .passwordController
+                                      .text,
+                                );
+                              }
+                            },
+                      child: const Text('Register'),
+                    ),
+                    SizedBox(height: 10.h),
+                    
+                    AppTextButton(
+                      text: 'OR Sign In',
+                      textStyle: AppStyles.font14Blue,
+                      onPressed: () {
+                        Navigator.pushNamed(context, Routers.signInRouter);
+                      },
+                    ),
+                  ],
+                );
+              },
+            ),
           ),
         ),
       ),
